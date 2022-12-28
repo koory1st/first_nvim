b4f70a7f5368d91690cf79c103ac05899164e3ab
koory1st <32436334@qq.com>
2022-12-28 22:02:40 +0800

    feat: nvim-tree

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -7,5 +7,12 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', 
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
 end)
